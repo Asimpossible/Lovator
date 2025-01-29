256f69a51040589231cc338552f1b3863bc324db
Asimpossible <asimgarasov@gmail.com>
2025-01-29 12:27:18 +0400

    Lovator-Love Calculator Established]

---
+++ lovator.js
@@ -0,0 +1,21 @@
+const namesForm = document.querySelector("form");
+const submitButton = document.querySelector("button");
+const result = document.querySelector(".result");
+const input1 = document.querySelector(".input1");
+const input2 = document.querySelector(".input2");
+
+function loveCalculation() {
+  let n = Math.random();
+  n = Math.floor(n * 100 + 1);
+  return n;
+}
+
+function calculatingResult(event) {
+  event.preventDefault();
+  result.textContent = loveCalculation();
+  submitButton.disabled = true;
+}
+
+namesForm.addEventListener("input", () => {
+  submitButton.disabled = !(input1.value.trim() && input2.value.trim());
+});
